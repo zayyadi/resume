43734a72e9d88b6452c0329af87cd3691a348037
Zayyad Musa <alias.co>
2022-01-26 12:37:54 +0100

    anothers

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,5 @@
 
 import os
-
 from pathlib import Path
 
 import environ
@@ -117,14 +116,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media')
 ]
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'mediafiles'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
